1ea8294d3c27f67d8018fa1c6e6dcd2457e79674
Jerry Rodriguez <jrvj01@hotmail.com>
2022-01-17 19:22:11 -0400

    4. creating a class, using snippets

---
+++ ACM/ACM.BL/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        public int CustomerID { get; private set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+
+        public string FullName => $"{FirstName} , {LastName}";
+    }
+}
