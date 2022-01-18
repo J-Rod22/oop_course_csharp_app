caabf78294e12ef57f1e106741747911dda7b396
Jerry Rodriguez <jrvj01@hotmail.com>
2022-01-18 19:13:23 -0400

    methods and constructors

--- ACM/ACM.BL/Customer.cs
+++ ACM/ACM.BL/Customer.cs
@@ -8,6 +8,16 @@ namespace ACM.BL
 {
     public class Customer
     {
+        public Customer()
+        {
+
+        }
+
+        public Customer(int customerId)
+        {
+            customerId = customerId;
+        }
+
         public int CustomerID { get; private set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
@@ -31,5 +41,52 @@ namespace ACM.BL
         }
 
         public static int InstanceCount { get; set; }
+
+        /// <summary>
+        /// Saves the current customer.
+        /// </summary>
+        /// <returns></returns>
+        public bool Save()
+        {
+            // Code that saves the defined customer
+            return true;
+        }
+
+        /// <summary>
+        /// Retrieve one customer
+        /// </summary>
+        /// <param name="customerId"></param>
+        /// <returns></returns>
+        public Customer Retrieve(int customerId)
+        {
+            // Code tat retrieves the defined customer
+
+            return new Customer();
+        }
+
+        /// <summary>
+        /// Retrieve all customers.
+        /// </summary>
+        /// <returns></returns>
+        public List<Customer> Retrieve()
+        {
+            // Code tat retrieves the defined customer
+
+            return new List<Customer>();
+        }
+
+        /// <summary>
+        /// Validates the customer data.
+        /// </summary>
+        /// <returns></returns>
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (string.IsNullOrWhiteSpace(LastName)) isValid = false;
+            if (string.IsNullOrWhiteSpace(EmailAddress)) isValid = false;
+
+            return isValid;
+        }
     }
 }
