9642d9beb75763fc28064b30ebabc419aafd815c
Jerry Rodriguez <jrvj01@hotmail.com>
2022-01-18 19:27:55 -0400

    order classes

---
+++ ACM/ACM.BL/Order.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Order
+    {
+        public Order()
+        {
+
+        }
+
+        public Order(int orderId)
+        {
+            OrderId = orderId;
+        }
+
+        public DateTimeOffset? OrderDate { get; set; }
+
+        public int OrderId { get; private set; }        
+
+        /// <summary>
+        /// Saves the current order.
+        /// </summary>
+        /// <returns></returns>
+        public bool Save()
+        {
+            // Code that saves the defined order
+            return true;
+        }
+
+        /// <summary>
+        /// Retrieve one order
+        /// </summary>
+        /// <param name="orderId"></param>
+        /// <returns></returns>
+        public Order Retrieve(int orderId)
+        {
+            // Code tat retrieves the defined order
+
+            return new Order();
+        }
+
+        /// <summary>
+        /// Retrieve all orders.
+        /// </summary>
+        /// <returns></returns>
+        public List<Order> Retrieve()
+        {
+            // Code tat retrieves the defined order
+
+            return new List<Order>();
+        }
+
+        /// <summary>
+        /// Validates the order data.
+        /// </summary>
+        /// <returns></returns>
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (OrderDate == null) isValid = false;
+
+            return isValid;
+        }
+    }
+}
---
+++ ACM/ACM.BL/OrderItem.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class OrderItem
+    {
+        public OrderItem()
+        {
+
+        }
+
+        public OrderItem(int orderItemId)
+        {
+            OrderItemId = orderItemId;
+        }
+
+        public int OrderItemId { get; private set; }
+        public int ProductId { get; set; }
+        public decimal? PurchasePrices { get; set; }
+        public int Quantity { get; set; }
+
+        public bool Save()
+        {
+            // Code that saves the defined customer
+            return true;
+        }
+
+        /// <summary>
+        /// Retrieve one order item
+        /// </summary>
+        /// <param name="oderItemId"></param>
+        /// <returns></returns>
+        public OrderItem Retrieve(int oderItemId)
+        {
+            // Code tat retrieves the defined order item
+
+            return new OrderItem();
+        }
+
+        /// <summary>
+        /// Retrieve all order items.
+        /// </summary>
+        /// <returns></returns>
+        public List<OrderItem> Retrieve()
+        {
+            // Code tat retrieves the defined order item
+
+            return new List<OrderItem>();
+        }
+
+        /// <summary>
+        /// Validates the order item data.
+        /// </summary>
+        /// <returns></returns>
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (Quantity <= 0) isValid = false;
+            if (ProductId <= 0) isValid = false;
+            if (PurchasePrices == null) isValid = false;
+
+            return isValid;
+        }
+    }
+}
