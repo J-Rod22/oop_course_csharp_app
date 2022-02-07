9abe3859a3199dc40f472b915ae739227c7cca42
Jerry Rodriguez <jrvj01@hotmail.com>
2022-02-07 18:58:41 -0400

    building reusable components with static keyword

--- ACM/ACM.BL/Product.cs
+++ ACM/ACM.BL/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Acme.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,7 +22,18 @@ namespace ACM.BL
         public decimal? CurrentPrice { get; set; }
         public string ProductDescription { get; set; }
         public int ProductId { get; private set; }
-        public string ProductName { get; set; }
+        public string _productName { get; set; }
+        public string ProductName
+        {
+            get
+            {                
+                return StringHandler.InsertSpaces(_productName);
+            }
+            set 
+            {
+                _productName = value;
+            }
+        }
 
         /// <summary>
         /// Validates the product data.
